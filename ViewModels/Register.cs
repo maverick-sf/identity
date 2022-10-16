51547dd763feec00abbe0fb325f7ca017772ab39
maverick-sf <107636098+maverick-sf@users.noreply.github.com>
2022-10-16 22:45:03 +0530

    Add files via upload

---
+++ ViewModels/Register.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Identity_Demo1.ViewModels
+{
+    public class Register
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+       
+        
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password),ErrorMessage ="Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+
+    } 
+}
